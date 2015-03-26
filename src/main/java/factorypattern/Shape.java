08a73a6794d408312a5d4e03371a3a4050dba2de
ufukomer <ufukomer@hotmail.com>
2015-03-27 01:51:53 +0200

    enums added

--- src/main/java/factorypattern/Shape.java
+++ src/main/java/factorypattern/Shape.java
@@ -4,7 +4,7 @@ package main.java.factorypattern;
  * Created by semih on 27.03.2015.
  */
 public interface Shape {
-
+    
     void draw();
 
 }
---
+++ src/main/java/factorypattern/ShapeTypes.java
@@ -0,0 +1,15 @@
+package main.java.factorypattern;
+
+/**
+ * Created by Ufuk on 27-03-15.
+ */
+public enum ShapeTypes {
+
+    CIRCLE,
+    SQUARE,
+    TRIANGLE,
+    ELLIPSE,
+    RECTANGULAR,
+    HEXAGON
+    
+}
