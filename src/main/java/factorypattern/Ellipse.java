0ac84167b662b2bbaa8f823cc7ea8fd53e14fff8
semihokanpehlivan <semihokanp@gmail.com>
2015-03-27 02:28:33 +0200

    added the ellipse class

---
+++ src/main/java/factorypattern/Ellipse.java
@@ -0,0 +1,12 @@
+package main.java.factorypattern;
+
+/**
+ * Created by semih on 27.03.2015.
+ */
+public class Ellipse implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Draw ellipse");
+    }
+}
