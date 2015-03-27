6d869c52c3f14ed3ab181e24419280704527277a
ufukomer <ufukomer@hotmail.com>
2015-03-27 02:31:09 +0200

    factory completed

--- src/main/java/factorypattern/ShapeFactory.java
+++ src/main/java/factorypattern/ShapeFactory.java
@@ -4,14 +4,17 @@ package main.java.factorypattern;
  * Created by Ufuk on 27-03-15.
  */
 public class ShapeFactory {
-
     
     public static Shape create(ShapeTypes type) {
+
         if(ShapeTypes.CIRCLE == type)
             return new Circle();
-
+        else if(ShapeTypes.TRIANGLE == type)
+            return new Triangle();
+        else if(ShapeTypes.ELLIPSE == type)
+            return new Ellipse();
         return null;
-
+        
     }
 
 }
