0f4b3664039b108ee7ac0fb3b0faf83244566ff0
Ali Yasar <aliyasar04@gmail.com>
2015-03-27 03:02:04 +0200

    Draw Square

--- src/main/java/factorypattern/Square.java
+++ src/main/java/factorypattern/Square.java
@@ -3,5 +3,13 @@ package main.java.factorypattern;
 /**
  * Created by aliyasar on 27/03/15.
  */
+<<<<<<< Updated upstream
 public class Square {
+=======
+public class Square implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Draw Square.");
+    }
+>>>>>>> Stashed changes
 }
