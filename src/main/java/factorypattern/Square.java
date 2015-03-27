57f3467507f3079308d1e9366b3e5cb13c1a9a59
semihokanpehlivan <semihokanp@gmail.com>
2015-03-27 03:18:53 +0200

    fix square

--- src/main/java/factorypattern/Square.java
+++ src/main/java/factorypattern/Square.java
@@ -3,13 +3,11 @@ package main.java.factorypattern;
 /**
  * Created by aliyasar on 27/03/15.
  */
-<<<<<<< Updated upstream
-public class Square {
-=======
+
 public class Square implements Shape {
     @Override
     public void draw() {
         System.out.println("Draw Square.");
     }
->>>>>>> Stashed changes
+
 }
