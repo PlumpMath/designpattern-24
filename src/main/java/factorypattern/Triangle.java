a5e87ac74c6c4055d57b5081817093fa461c8d8b
ufukomer <ufukomer@hotmail.com>
2015-03-27 02:24:57 +0200

    Factory added

---
+++ src/main/java/factorypattern/Triangle.java
@@ -0,0 +1,12 @@
+package main.java.factorypattern;
+
+/**
+ * Created by Ufuk on 27-03-15.
+ */
+public class Triangle implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Draw circle");
+    }
+}
