c896c07ce8f71fa387136ecd56f17b896474d0cd
semihokanpehlivan <semihokanp@gmail.com>
2015-03-27 02:56:32 +0200

    added Regtangular

--- src/main/java/factorypattern/Rectangular.java
+++ src/main/java/factorypattern/Rectangular.java
@@ -3,5 +3,10 @@ package main.java.factorypattern;
 /**
  * Created by semih on 27.03.2015.
  */
-public class Rectangular {
+public class Rectangular  implements Shape{
+
+    @Override
+    public void draw() {
+        System.out.println("Draw Rectangular");
+    }
 }
