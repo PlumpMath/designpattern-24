5d43a8b3bf31465b5c234e4e09229cf52bd19016
ufukomer <ufukomer@hotmail.com>
2015-03-27 02:16:14 +0200

    Factory added

---
+++ src/main/java/factorypattern/ShapeFactory.java
@@ -0,0 +1,15 @@
+package main.java.factorypattern;
+
+/**
+ * Created by Ufuk on 27-03-15.
+ */
+public class ShapeFactory {
+
+    private static Shape shape;
+
+    Shape create(ShapeTypes type) {
+        if(ShapeTypes.CIRCLE == type)
+
+    }
+
+}
